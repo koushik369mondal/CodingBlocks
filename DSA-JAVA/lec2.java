7249e084ef8fe926f3df660bd0864c227362f4af
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-03 10:09:16 +0530

    Refactor main method to call mulTable and comment out count method

--- DSA-JAVA/lec2.java
+++ DSA-JAVA/lec2.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class lec2 {
+    // Question 1
     public static void add(){
         int x = 5;
         int y = 6;
@@ -8,6 +9,7 @@ public class lec2 {
         System.out.println(z);
     }
 
+    // Question 2
     public static void simpleInterest(){
         int p = 1000;
         int r = 5;
@@ -16,6 +18,7 @@ public class lec2 {
         System.out.println(si);
     }
 
+    // Question 3
     public static void oddEven(){
         System.out.print("Enter a number: ");
         Scanner sc = new Scanner(System.in);
@@ -27,6 +30,7 @@ public class lec2 {
         }
     }
 
+    // Question 4
     public static void max(){
         int a = 10;
         int b = 20;
@@ -37,6 +41,7 @@ public class lec2 {
         }
     }
 
+    // Question 5
     public static void grade(){
         int marks = 85;
         if(marks >= 90){
@@ -50,10 +55,21 @@ public class lec2 {
         }
     }
 
+    // Question 6
     public static void count(){
         int n = 5;
         for(int i=1; i<=n; i++){
             System.out.print(i + " ");
+        }
+    }
+
+    // Question 7
+    public static void mulTable(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        for(int i=1; i<=10; i++){
+            System.out.println(n + " x " + i + " = " + (n * i));
         }
     }
 
@@ -63,6 +79,7 @@ public class lec2 {
         // oddEven();
         // max();
         // grade();
-        count();
+        // count();
+        mulTable();
     }
 }
