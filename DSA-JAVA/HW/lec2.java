8bf82fe01e64706941e35bf2ae486909da542483
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-03 19:47:40 +0530

    Implement sumOdd method to calculate and print the sum of odd numbers up to 5

--- DSA-JAVA/HW/lec2.java
+++ DSA-JAVA/HW/lec2.java
@@ -1,8 +1,18 @@
 package HW;
 public class lec2 {
 
-    
+    public static void sumOdd(){
+        int n = 5;
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+            if(i % 2 !=0){
+                sum = sum + i;
+            }
+        }
+        System.out.println("Sum of odd numbers: " + sum);
+    }
+
     public static void main(String[] args) {
-        
+        sumOdd();
     }
 }
