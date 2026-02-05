aa7911771428fb502a02f0084865ea8c720003ae
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-05 09:38:25 +0530

    pattern2

--- DSA-JAVA/CW/lec4.java
+++ DSA-JAVA/CW/lec4.java
@@ -24,7 +24,18 @@ public class lec4 {
         }
     }
 
+    public static void pattern2(){
+        int n = 5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        pattern1();
+        // pattern1();
+        pattern2();
     }
 }
