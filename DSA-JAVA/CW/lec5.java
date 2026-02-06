b2a55eabaf3cda91370d6444aa6c1a68844f8fc4
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-06 10:13:09 +0530

    Add pattern3 method for alternating star and space output

--- DSA-JAVA/CW/lec5.java
+++ DSA-JAVA/CW/lec5.java
@@ -54,8 +54,41 @@ public class lec5 {
         }
     }
 
+    public static void pattern3(){
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        int n = 5;
+        int space = n - 1;
+        int star = 1;
+        int row = 1;
+        while (row <= n) {
+            // space
+            int i = 1;
+            while (i <= space) {
+                System.out.print("  ");
+                i++;
+            }
+            // star
+            int j = 1;
+            while (j <= star) {
+                if (j % 2 == 1) {
+                    System.out.print("* ");
+                    j++;
+                } else {
+                    System.out.print("  ");
+                    j++;
+                }
+            }
+            System.out.println();
+            row++;
+            space--;
+            star += 2;
+        }
+    }
+
     public static void main(String[] args) {
         // pattern1();
-        pattern2();
+        // pattern2();
+        pattern3();
     }
 }
