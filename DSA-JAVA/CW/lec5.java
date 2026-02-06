2dafe1dee8d1ea98c8f40fd7864acb3d57922e2f
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-06 10:05:42 +0530

    pattern2

--- DSA-JAVA/CW/lec5.java
+++ DSA-JAVA/CW/lec5.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
-public class lec5{
-    public static void pattern1(){
+public class lec5 {
+    public static void pattern1() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int space = n - 1;
@@ -26,7 +26,36 @@ public class lec5{
             star += 2;
         }
     }
+
+    public static void pattern2() {
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        int n = 5;
+        int space = 0;
+        int star = 2 * n - 1;
+        int row = 1;
+        while (row <= n) {
+            // space
+            int i = 1;
+            while (i <= space) {
+                System.out.print("  ");
+                i++;
+            }
+            // star
+            int j = 1;
+            while (j <= star) {
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println();
+            row++;
+            space++;
+            star = star - 2;
+        }
+    }
+
     public static void main(String[] args) {
-        pattern1();
+        // pattern1();
+        pattern2();
     }
 }
