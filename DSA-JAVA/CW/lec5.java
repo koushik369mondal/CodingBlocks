58fbafd05863fd535a2f24cab8996664b743ee08
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-06 09:43:48 +0530

    Refactor lec5: Move main method to the end and improve formatting in pattern1

--- DSA-JAVA/CW/lec5.java
+++ DSA-JAVA/CW/lec5.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
 
 public class lec5{
-    public static void main(String[] args) {
+    public static void pattern1(){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int space = n-1;
+        int space = n - 1;
         int star = 1;
         int row = 1;
-        while(row<=n){
-            //space 
+        while (row <= n) {
+            // space
             int i = 1;
-            while(i<=space){
+            while (i <= space) {
                 System.out.print("  ");
                 i++;
             }
-            //star
-            int j=1;
-            while(j<=star) {
+            // star
+            int j = 1;
+            while (j <= star) {
                 System.out.print("* ");
                 j++;
             }
             System.out.println();
             row++;
             space--;
-            star+=2;
+            star += 2;
         }
+    }
+    public static void main(String[] args) {
+        pattern1();
     }
 }
