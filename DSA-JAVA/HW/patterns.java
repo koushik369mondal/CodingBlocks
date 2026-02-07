1a55e4a9ecc8bdfe3ac1552666ec09180d8c1c53
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-07 20:34:09 +0530

    right-angled triangle star pattern using while loop

--- DSA-JAVA/HW/patterns.java
+++ DSA-JAVA/HW/patterns.java
@@ -40,12 +40,24 @@ public class patterns {
     public static void pattern2(){
         int n = 5;
 
-        System.out.println("Usinng For Loop");
+        System.out.println("Usinng For Loop : ");
         for(int i=1; i<=n; i++){
             for(int j=1; j<=i; j++){
                 System.out.print(" * ");
             }
             System.out.println();
+        }
+
+        System.out.println("Using While Loop : ");
+        int row = 1;
+        while(row<=n){
+            int i = 1;
+            while(i <= row){
+                System.out.print(" * ");
+                i++;
+            }
+            System.out.println();
+            row++;
         }
     }
 
