01d4be9ca08ed64cf46171a6d47e63d96c4330f9
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-07 16:41:50 +0530

    Add pattern1 method to generate a square star pattern

--- DSA-JAVA/HW/patterns.java
+++ DSA-JAVA/HW/patterns.java
@@ -2,4 +2,21 @@ package HW;
 
 public class patterns {
     
+    // * * * * *
+    // * * * * *
+    // * * * * *
+    // * * * * *
+    // * * * * *
+    public static void pattern1(){
+        int n = 5;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern1();
+    }
 }
