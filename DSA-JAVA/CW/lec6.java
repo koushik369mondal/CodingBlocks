0f478b8c498c985eb168a9c04ef142aa41accae6
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-09 10:17:31 +0530

    Add pattern3 method to generate a diamond star pattern using for and while loops

--- DSA-JAVA/CW/lec6.java
+++ DSA-JAVA/CW/lec6.java
@@ -82,8 +82,50 @@ public class lec6 {
         }
     }
 
+    //         * * * * *
+    //       * * * *
+    //     * * *
+    //   * *
+    // *
+    //   * *
+    //     * * *
+    //       * * *
+    //         * * * * *
+    public static void pattern3(){
+        int n = 5;
+
+        System.out.println("Using For Loop : ");
+
+        System.out.println("Using While Loop : ");
+        int row = 1;
+        int space = n - 1;
+        int star = n;
+        while (row <= 2 * n - 1) {
+            int i = 1;
+            while (i <= space) {
+                System.out.print("  ");
+                i++;
+            }
+            int j = 1;
+            while (j <= star) {
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println();
+            if (row < n) {
+                space--;
+                star--;
+            } else {
+                space++;
+                star++;
+            }
+            row++;
+        }
+    }
+
     public static void main(String[] args) {
-        pattern1();
-        pattern2();
+        // pattern1();
+        // pattern2();
+        pattern3();
     }
 }
