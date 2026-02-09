01854883b592b6cb60568f7913e91c0bd2264b7c
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-09 20:20:21 +0530

    Add pattern19 method to generate a number pyramid using while loop

--- DSA-JAVA/HW/patterns.java
+++ DSA-JAVA/HW/patterns.java
@@ -308,7 +308,34 @@ public class patterns {
     //         5   6   7   8   9
     //     10  11  12  13  14  15  16
     // 17  18  19  20  21  22  23  24  25
-    public static void pattern19(){}
+    public static void pattern19(){
+        int n = 5;
+        int num = 1;
+
+        System.out.println("Using While Loop : ");
+        int space = n - 1;
+        int star = 1;
+        int row = 1;
+        while (row <= n) {
+            // space
+            int i = 1;
+            while (i <= space) {
+                System.out.print("  ");
+                i++;
+            }
+            // star
+            int j = 1;
+            while (j <= star) {
+                System.out.print(num + " ");
+                num++;
+                j++;
+            }
+            System.out.println();
+            row++;
+            space--;
+            star += 2;
+        }
+    }
 
     // *       * * * * *
     // *       *
@@ -340,8 +367,8 @@ public class patterns {
         // pattern15();
         // pattern16();
         // pattern17();
-        pattern18();
-        // pattern19();
+        // pattern18();
+        pattern19();
         // pattern20();
     }
 }
