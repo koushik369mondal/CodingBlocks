a9876f4fc57a2223342d8f508c516ac22acc865f
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-11 09:45:37 +0530

    printPrimeNumbers method to improve prime number generation logic in lec8.java

--- DSA-JAVA/CW/lec8.java
+++ DSA-JAVA/CW/lec8.java
@@ -21,9 +21,25 @@ public class lec8 {
     }
 
 
-    
+    public static void printPrimeNumbers(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=2; i<=n; i++){
+            boolean isPrime = true;
+            for(int j=2; j<i; j++){
+                if(i%j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.print(i + " ");
+            }
+        }
+    }
 
     public static void main(String[] args) {
-        isPrime();
+        // isPrime();
+        printPrimeNumbers();
     }
 }
