c0e339a2832f7e2d8abcc1e87b07181306157883
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-14 18:24:34 +0530

    Add initial implementation of pattern generation methods in ass2 class

---
+++ DSA-JAVA/HW/ass2.java
@@ -0,0 +1,121 @@
+package HW;
+
+public class ass2 {
+    //  * * * * *
+    //  * *   * *
+    //  *       *
+    //  * *   * *
+    //  * * * * *
+    public static void pattern1(){}
+
+    //       1 
+    //     2 3 2 
+    //   3 4 5 4 3 
+    // 4 5 6 7 6 5 4
+    public static void pattern2(){}
+
+    //       *
+    //    *  *  *  
+    // *  *  *  *  *  
+    //    *  *  *
+    //       *
+    public static void pattern3(){}
+
+    // 1 
+    // 2 3 
+    // 4 5 6 
+    // 7 8 9 10
+    public static void pattern4(){}
+
+    //     *****
+    //    *   *
+    //   *   *
+    //  *   *
+    // *****
+    public static void pattern5(){}
+
+    //     1 
+    //   2 3 2 
+    // 3 4 5 4 3 
+    //   2 3 2 
+    //     1
+    public static void pattern6(){}
+
+    // 1 
+    // 2 2 
+    // 3 0 3 
+    // 4 0 0 4 
+    // 5 0 0 0 5
+    public static void pattern7(){}
+
+    // 0 
+    // 1 1 
+    // 2 3 5 
+    // 8 13 21 34
+    public static void pattern8(){}
+
+    // 1           1 
+    // 1 2       2 1 
+    // 1 2 3   3 2 1 
+    // 1 2 3 4 3 2 1
+    public static void pattern9(){}
+
+    // *********
+    // **** ****
+    // ***   ***
+    // **     **
+    // *       *
+    // **     **
+    // ***   ***
+    // **** ****
+    // *********
+    public static void pattern10(){}
+
+    //             1 
+    //         2 1   1 2 
+    //     3 2 1       1 2 3 
+    // 4 3 2 1           1 2 3 4 
+    //     3 2 1       1 2 3 
+    //         2 1   1 2 
+    //             1
+    public static void pattern11(){}
+
+    // 1 
+    // 1 1 
+    // 1 2 1 
+    // 1 3 3 1 
+    // 1 4 6 4 1 
+    // 1 5 10 10 5 1
+    public static void pattern12(){}
+
+    // 5                   5 
+    // 5 4               4 5 
+    // 5 4 3           3 4 5 
+    // 5 4 3 2       2 3 4 5 
+    // 5 4 3 2 1   1 2 3 4 5 
+    // 5 4 3 2 1 0 1 2 3 4 5 
+    // 5 4 3 2 1   1 2 3 4 5 
+    // 5 4 3 2       2 3 4 5 
+    // 5 4 3           3 4 5 
+    // 5 4               4 5 
+    // 5                   5
+    public static void pattern13(){}
+
+    // 5 4 3 2 1 0 1 2 3 4 5 
+    //   4 3 2 1 0 1 2 3 4 
+    //     3 2 1 0 1 2 3 
+    //       2 1 0 1 2 
+    //         1 0 1 
+    //           0 
+    //         1 0 1 
+    //       2 1 0 1 2 
+    //     3 2 1 0 1 2 3 
+    //   4 3 2 1 0 1 2 3 4 
+    // 5 4 3 2 1 0 1 2 3 4 5
+    public static void pattern14(){}
+
+
+    public static void main(String[] args) {
+        
+    }
+}
