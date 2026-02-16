6ee109c7ba54fac3ef646bbb60483e0573276e2c
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-16 10:03:13 +0530

    Refactor printArray method; improve formatting and structure of findElement method

--- DSA-JAVA/CW/lec10.java
+++ DSA-JAVA/CW/lec10.java
@@ -3,17 +3,40 @@ package CW;
 import java.util.Scanner;
 
 public class lec10 {
-    public static void main(String[] args) {
+    public static void printArray(int n){
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[5];
-        
+        System.out.println("Enter 5 integers: ");
+        for (int i = 0; i < 5; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The array elements are: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void findElement(){
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[5];
+        int key = 0;
         System.out.println("Enter 5 integers: ");
         for(int i=0; i<5; i++){
             arr[i] = sc.nextInt();
         }
-        System.out.println("The array elements are: ");
+        System.out.println("Enter the element to be searched: ");
+        key = sc.nextInt();
         for(int i=0; i<5; i++){
-            System.out.print(arr[i] + " ");
+            if(arr[i] == key){
+                System.out.println("Element found at index: " + i);
+                return;
+            }
         }
+        System.out.println("Element not found");
+    }
+
+    public static void main(String[] args) {
+        // printArray(5);
+        findElement();
     }
 }
