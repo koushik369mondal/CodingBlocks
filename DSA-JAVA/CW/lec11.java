9160467c9b014a8d7ccbc41f7f751b079c1769f2
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-18 09:55:51 +0530

    Refactor lec11 to include swap methods and improve formatting; enhance main method for better readability.

--- DSA-JAVA/CW/lec11.java
+++ DSA-JAVA/CW/lec11.java
@@ -24,13 +24,43 @@ public class lec11 {
         System.out.println(min);
         return min;
     }
+
+    public static void swaptemp(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a: " + a + " b: " + b);
+    }
+
+    public static void swapwithouttemp(int a, int b) {
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a: " + a + " b: " + b);
+    }
+
+    public static void swapXOR(int a, int b) {
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+        System.out.println("a: " + a + " b: " + b);
+    }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int arr[] = new int[5];
-        for(int i=0; i<arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
         System.out.println("Max: " + arrayMax(arr));
         System.out.println("Min: " + arrayMin(arr));
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        swaptemp(a, b);
+        swapwithouttemp(a, b);
+        swapXOR(a, b);
+
+        sc.close();
     }
 }
