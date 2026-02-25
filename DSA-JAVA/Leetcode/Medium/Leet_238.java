a613ef5754eccd5c7b45249145bdd7a64f990f8a
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-26 01:18:26 +0530

    Implement productExceptSelf method in Leet_238 and update main method to display results; remove unused productExceptSelf method from Main; add Leet_134 class skeleton

--- DSA-JAVA/Leetcode/Medium/Leet_238.java
+++ DSA-JAVA/Leetcode/Medium/Leet_238.java
@@ -1,8 +1,29 @@
 // 238. Product of Array Except Self
 package Leetcode.Medium;
+import java.util.Arrays;
 public class Leet_238 {
+
+    public int[] productExceptSelf(int[] nums){
+        int n = nums.length;
+        int[] result = new int[n];
+        // Left Product
+        result[0]=1;
+        for(int i=1; i<n; i++){
+            result[i] = result[i-1] * nums[i-1];
+        }
+        // Right Product
+        int right = 1;
+        for(int i=n-1; i>=0; i--){
+            result[i] = result[i] * right;
+            right = right * nums[i];
+        }
+        return result;
+    }
     
     public static void main(String[] args) {
-        
+        int[] nums = {1, 2, 3, 4};
+        Leet_238 leet = new Leet_238();
+        int[] res = leet.productExceptSelf(nums);
+        System.out.println(Arrays.toString(res));
     }
 }
