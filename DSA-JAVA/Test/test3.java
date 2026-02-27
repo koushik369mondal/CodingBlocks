666328696980b37e7ec271963320ba7fe3cbb15c
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-27 23:53:00 +0530

    Implement insertion sort method and update main to call it; comment out moveZeroes method

--- DSA-JAVA/Test/test3.java
+++ DSA-JAVA/Test/test3.java
@@ -72,6 +72,22 @@ public class test3 {
     }
 
     // Insersion sort
+    public static void insertionSort(){
+        int arr[] = {12, 11, 13, 5, 6};
+        int n = arr.length;
+        for(int i=1; i<n; i++){
+            int key = arr[i];
+            int j = i - 1;
+            while(j >= 0 && arr[j] > key){
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
 
     //       1
     //     2 3 2
@@ -88,6 +104,7 @@ public class test3 {
         // missingNumber();
         // trappingRainWater();
         // System.out.println(duplicateNumber());
-        moveZeroes();
+        // moveZeroes();
+        insertionSort();
     }
 }
