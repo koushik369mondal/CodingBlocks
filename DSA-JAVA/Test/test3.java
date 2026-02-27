f0b1d1c61b786a9ffc2311678b03af30c746e1ec
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-27 19:28:33 +0530

    Refactor test3 class: Uncomment trappingRainWater method and comment out missingNumber call in main

--- DSA-JAVA/Test/test3.java
+++ DSA-JAVA/Test/test3.java
@@ -1,6 +1,7 @@
 package Test;
 
 public class test3 {
+
     // Find missing number in an array
     public static void missingNumber(){
         int arr[] = {3, 0, 1};
@@ -13,7 +14,30 @@ public class test3 {
         System.out.println(sum1 - sum2);
     }
 
-    // Trapping Rain Water                                                                                     
+    // Trapping Rain Water
+    public static void trappingRainWater(){
+        int height[] = {4,2,0,3,2,5};
+        int n = height.length;
+
+        int leftMax[] = new int[n];
+        int rightMax[] = new int[n];
+        // left max
+        leftMax[0] = height[0];
+        for(int i=1; i<n; i++){
+            leftMax[i] = Math.max(leftMax[i-1], height[i]);
+        }
+        // right max
+        rightMax[n-1] = height[n-1];
+        for(int i=n-2; i>=0; i--){
+            rightMax[i] = Math.max(rightMax[i+1], height[i]);
+        }
+
+        int trappedWater = 0;
+        for(int i=0; i<n; i++){
+            trappedWater += Math.min(leftMax[i], rightMax[i]) - height[i];
+        }
+        System.out.println(trappedWater);
+    }                                                                               
 
     // duplicate number in an array
 
@@ -33,6 +57,7 @@ public class test3 {
     //                 *
 
     public static void main(String[] args) {
-        missingNumber();
+        // missingNumber();
+        trappingRainWater();
     }
 }
