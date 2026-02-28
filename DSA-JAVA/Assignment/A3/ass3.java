7bb44e00a14c96e60551b221a5a90c510d34cb4e
Kaushik Mandal <koushik369mondal@gmail.com>
2026-02-28 20:53:45 +0530

    Fix printReverse method to remove extra space in output

--- DSA-JAVA/Assignment/A3/ass3.java
+++ DSA-JAVA/Assignment/A3/ass3.java
@@ -47,9 +47,9 @@ public class ass3 {
     public static void printReverse(){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        while(n > 0){
+        while (n > 0) {
             int rem = n % 10;
-            System.out.print(rem + " ");
+            System.out.print(rem);
             n = n / 10;
         }
         sc.close();
