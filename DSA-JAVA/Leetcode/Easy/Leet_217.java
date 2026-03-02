f7d5c031ca3698b8cad04b6b079ebe3619b6ea39
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-02 10:53:58 +0530

    Refactor Leet_217 class to improve containsDuplicate method and add containsDuplicate2 for better performance

--- DSA-JAVA/Leetcode/Easy/Leet_217.java
+++ DSA-JAVA/Leetcode/Easy/Leet_217.java
@@ -1,6 +1,10 @@
+// 217. Contains Duplicate
 package Leetcode.Easy;
 
+import java.util.Arrays;
+
 public class Leet_217 {
+    // Time Limit Exceeded
     public boolean containsDuplicate(int[] nums) {
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
@@ -10,11 +14,22 @@ public class Leet_217 {
             }
         }
         return false;
+    }
 
+    // 25ms | Beats 13.52% 
+    public boolean containsDuplicate2(int[] nums){
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
     }
     public static void main(String[] args) {
             Leet_217 obj = new Leet_217();
             int nums[] = {1,2,3,1};
-            System.out.println(obj.containsDuplicate(nums));
+            // System.out.println(obj.containsDuplicate(nums));
+            System.out.println(obj.containsDuplicate2(nums));
     }
 }
