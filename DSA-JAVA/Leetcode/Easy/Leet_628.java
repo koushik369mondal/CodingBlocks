90276ff210cc41aea6a4b7ff27e6383752afc4f1
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-03 01:33:39 +0530

    Add Leet_628 class to implement maximumProduct method for calculating maximum product of three numbers in an array

---
+++ DSA-JAVA/Leetcode/Easy/Leet_628.java
@@ -0,0 +1,19 @@
+package Leetcode.Easy;
+
+import java.util.Arrays;
+
+public class Leet_628 {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int p1 = nums[n-1] * nums[n-2] * nums[n-3];
+        int p2 = nums[0] * nums[1] * nums[n-1];
+        return Math.max(p1, p2);
+    }
+
+    public static void main(String[] args) {
+        Leet_628 obj = new Leet_628();
+        int[] nums = { -100, -98, -1, 2, 3, 4};
+        System.out.println(obj.maximumProduct(nums));
+    }
+}
