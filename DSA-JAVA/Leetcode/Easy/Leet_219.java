4c5bbb9ed405ac72e55d8d0fd4c2466d77b13ab8
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-02 12:54:24 +0530

    Add Leet_219 class to implement containsNearbyDuplicate methods for checking nearby duplicates in an array

---
+++ DSA-JAVA/Leetcode/Easy/Leet_219.java
@@ -0,0 +1,34 @@
+package Leetcode.Easy;
+
+public class Leet_219 {
+    // Time Limit Exceeded
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j] && Math.abs(i - j) <= k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // 1173ms | Beats 6.39%
+    public boolean containsNearbyDuplicate2(int[] nums, int k) {
+        for (int i=0; i<nums.length; i++) {
+            for (int j=i+1; j<nums.length && j<=i+k ; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Leet_219 obj = new Leet_219();
+        int[] nums = { 1, 2, 3, 1, 2, 3};
+        // System.out.println(obj.containsNearbyDuplicate(nums, 3));
+        System.out.println(obj.containsNearbyDuplicate2(nums, 2));
+    }
+}
