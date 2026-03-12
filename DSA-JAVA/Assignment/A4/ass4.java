542f941e689579a038d58c71326a8785fda71b09
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-12 20:34:10 +0530

    add ass4 class to implement inverse of an array functionality

---
+++ DSA-JAVA/Assignment/A4/ass4.java
@@ -0,0 +1,73 @@
+package Assignment.A4;
+
+import java.util.Scanner;
+
+public class ass4 {
+    // Inverse of an array
+    public static void inverseOfArray(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int[] inverse = new int[n];
+        for(int i=0; i<n; i++){
+            inverse[arr[i]] = i;
+        }
+        for(int i=0; i<n; i++){
+            System.out.print(inverse[i] + " ");
+        }
+        sc.close();
+    }
+
+    // Arrays-Target Sum Triplets
+
+    // Arrays-Linear Search
+
+    // Arrays-Target Sum Pairs
+
+    // Arrays-Max Value In Array
+
+    // Pair of Roses
+
+    // Help Ramu
+
+    // Alex Goes Shopping
+
+    // Arrays-Reverse an Array
+
+    // Squares of a Sorted Array
+
+    // Sort just Zeroes and Ones
+
+    // Calculate The Sum
+
+    // Arrays-Bubble Sort
+
+    // Maximum Sum Path in Two Arrays
+
+    // Arrays-Selection Sort
+
+    // Arrays-Insertion Sort
+
+    // Maximum Circular Sum
+
+    // Sorting Patient Severity Levels in Hospitals
+
+    // Product of Array Except Self
+
+    // Running Sum of the Array
+
+    // Von Neuman Loves Binary
+
+    // Rain Water Trapping
+
+    // Majority Element
+
+    // Next Permutation
+
+    public static void main(String[] args) {
+        inverseOfArray();
+    }
+}
