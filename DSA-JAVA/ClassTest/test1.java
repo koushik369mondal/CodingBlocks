227f2dea96cab0ee227e4a74d7821675c9bdb442
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-19 01:45:39 +0530

    Add main method to test1 class for target sum pair identification

--- DSA-JAVA/ClassTest/test1.java
+++ DSA-JAVA/ClassTest/test1.java
@@ -1,2 +1,24 @@
 package ClassTest;
 
+import java.util.Scanner;
+
+public class test1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        sc.close();
+        int target = sc.nextInt();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(arr[i] + arr[j] == target){
+                    System.out.print(arr[i] + "," + arr[j]);
+                    return;
+                }
+            }
+        }
+    }
+}
