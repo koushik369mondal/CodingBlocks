cfda14bc901834e03133d037c78e16b2476655db
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-21 17:50:03 +0530

    Implement rightShift method to rotate an array by one position to the right

--- DSA-JAVA/ClassTest/test4.java
+++ DSA-JAVA/ClassTest/test4.java
@@ -11,7 +11,8 @@ public class test4 {
         // trappingRainWater();
         // productOfArrayExceptSelf();
         // findMedian();
-        runningSum();
+        // runningSum();
+        rightShift();
     }
 
     // rotate array by k times
@@ -195,5 +196,21 @@ public class test4 {
     }
 
     // right shift by 1
-
+    public static void rightShift(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int last = arr[n-1];
+        for(int i=n-1; i>0; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = last;
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
 }
