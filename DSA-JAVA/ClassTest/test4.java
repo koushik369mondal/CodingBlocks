ba36cfcc5aff0812949f10be3ab7d703e7c28ad5
Kaushik Mandal <koushik369mondal@gmail.com>
2026-03-21 10:39:37 +0530

    Add productOfArrayExceptSelf and findMedian methods to test4 class

--- DSA-JAVA/ClassTest/test4.java
+++ DSA-JAVA/ClassTest/test4.java
@@ -1,5 +1,6 @@
 package ClassTest;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class test4 {
@@ -7,7 +8,9 @@ public class test4 {
         // rotate();
         // inverseOfArray();
         // maxSum();
-        trappingRainWater();
+        // trappingRainWater();
+        // productOfArrayExceptSelf();
+        findMedian();
     }
 
     // rotate array by k times
@@ -110,8 +113,66 @@ public class test4 {
     }
 
     // product of array except self
+    public static void productOfArrayExceptSelf(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int prefix[] = new int[n];
+        int suffix[] = new int[n];
+        prefix[0] = 1;
+        for(int i=1; i<n; i++){
+            prefix[i] = prefix[i-1] * arr[i-1];
+        }
+        suffix[n-1] = 1;
+        for(int i=n-2; i>=0; i--){
+            suffix[i] = suffix[i+1] * arr[i+1];
+        }
+        for(int i=0; i<n; i++){
+            System.out.println(prefix[i] * suffix[i]);            
+        }
+        sc.close();
+    }
 
     // median of two sorted arrays
+    public static void findMedian(){
+        Scanner sc = new Scanner(System.in);
+        // 1st arr
+        int n1 = sc.nextInt();
+        int arr1[] = new int[n1];
+        for(int i=0; i<n1; i++){
+            arr1[i] = sc.nextInt();
+        }
+        // 2nd arr
+        int n2 = sc.nextInt();
+        int arr2[] = new int[n2];
+        for(int i=0; i<n2; i++){
+            arr2[i] = sc.nextInt();
+        }
+        // merge
+        int merge[] = new int[n1+n2];
+        int k=0; // pointer for merge array
+        for(int i=0; i<n1; i++){
+            merge[k] = arr1[i];
+            k++;
+        }
+        for(int i=0; i<n2; i++){
+            merge[k] = arr2[i];
+            k++;
+        }
+        // sort
+        Arrays.sort(merge);
+        // median
+        int n = merge.length;
+        if(n % 2 == 0){ // even
+            System.out.println((merge[n/2] + merge[n/2 - 1]) / 2.0);
+        } else { // odd
+            System.out.println(merge[n/2]);
+        }
+        sc.close();
+    }
 
     // running sum of an array
 
